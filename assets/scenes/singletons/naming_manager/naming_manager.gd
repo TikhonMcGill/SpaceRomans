4ae6f3e240c829bdc18c8c32bd2f51923e7c5ecb
Tikhon McGill <tm2008@hw.ac.uk>
2024-03-28 15:59:10 +0000

    Implement Random Planet Naming Function

--- assets/scenes/singletons/naming_manager/naming_manager.gd
+++ assets/scenes/singletons/naming_manager/naming_manager.gd
@@ -11,5 +11,33 @@ extends Node
 func generate_ship_name() -> String:
 	return "RNS \"%s\"" % latin_words.pick_random()
 
-func _ready() -> void:
-	print(generate_ship_name())
+func generate_planet_name() -> String:
+	var vowels = ["a","e","i","u","a","e","i","u","ae","ei"]
+	var consonants = ["b","c","d","ph","l","ll","sc","r","m","n","t","p","qu","v"]
+	
+	var result : String = ""
+	var is_vowel = [true,false].pick_random()
+	
+	var name_size := randi_range(4,8)
+	
+	for x in range(name_size):
+		if is_vowel == true:
+			if x == name_size-1:
+				result += ["ia","ium","a","um","i","ii"].pick_random()
+			else:
+				result += vowels.pick_random()
+		else:
+			result += consonants.pick_random()
+			if x == name_size-1:
+				result += ["ia","ium","a","um","i","ii","us"].pick_random()
+		
+		is_vowel = not is_vowel
+	
+	while result.contains("iii") == true:
+		result = result.replace("iii","ii")
+	
+	if result.begins_with("ll") == true:
+		result = result.trim_prefix("ll")
+		result = "l" + result
+	
+	return result.capitalize()
