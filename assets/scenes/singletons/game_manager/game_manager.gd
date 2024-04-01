3d614b3024bb9d7a0dfc96df433753092b2fe2bf
Tikhon McGill <tm2008@hw.ac.uk>
2024-04-01 16:34:25 +0100

    Create BASE speed variables to explain modifiers

--- assets/scenes/singletons/game_manager/game_manager.gd
+++ assets/scenes/singletons/game_manager/game_manager.gd
@@ -1,5 +1,8 @@
 extends Node
 
+const BASE_ENEMY_SPEED : int = 200
+const BASE_ENEMY_PATROL_SPEED : int = 100
+
 var player_skin_color : Color = CharacterGraphic.HUMAN_SKIN_COLORS.pick_random() #The Color of the Player's skin in Customization
 var player_clothing_color : Color = Color.WEB_GRAY #The Color of the Player's clothes
 var player_hat : CharacterHat = null #The Hat the Player is wearing
@@ -12,6 +15,9 @@ var score : int = 0 #Score
 var missions : Array[Mission] = []
 
 var mission : Mission = null
+
+var effective_enemy_speed : int = BASE_ENEMY_SPEED
+var effective_enemy_patrol_speed : int = BASE_ENEMY_PATROL_SPEED
 
 func _ready() -> void:
 	_load_score()
